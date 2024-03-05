9b5629420509b733e6ebf66d0ccbe7de28cb181f
daniel <danielcooder@gmail.com>
2024-03-05 14:49:08 -0300

    perf: inicio de js

---
+++ js/Favorites.js
@@ -0,0 +1,15 @@
+//classe que vai conter a logica dos dados
+//como os dados serão estruturados
+
+export class Favorites {
+    constructor(root) {
+
+    }
+}
+
+//classe que vai criar a vizulização e eventos HTML
+export class FavoritesView extends Favorites {
+    constructor(root) {
+        super(root)
+    }
+}
